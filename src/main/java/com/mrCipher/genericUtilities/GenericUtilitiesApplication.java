cbf8ce416f5d398b4e9979909ff77967a3d90812
Shreyas Talole <shreyastalole@gmail.com>
2024-08-15 12:43:31 +0530

    First Commit
    pushing Spring Initialisation code

---
+++ src/main/java/com/mrCipher/genericUtilities/GenericUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.mrCipher.genericUtilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericUtilitiesApplication.class, args);
+	}
+
+}
